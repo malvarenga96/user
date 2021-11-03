f1a1b779ad5dfc90c1b96f447f49625145ac7729
Marvin Alvarenga <malvarenga@applaudostudios.com>
2021-11-03 08:42:55 -0600

    Configure the setup file for the project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='user',
+    version='0.1.0',
+    description='User example',
+    author='Marvin Alvarenga',
+    packages=find_packages(exclude=['tests']),
+)
